7efcb44f5cffded03f7676f4b2ca63c85f510ca9
Michael Kalygin <michael.kalygin@gmail.com>
2015-10-01 16:28:49 +0300

    add main provisioner methods

--- lib/vagrant-turbo/provisioner.rb
+++ lib/vagrant-turbo/provisioner.rb
@@ -1,6 +1,17 @@
 module VagrantPlugins
   module Turbo
     class Provisioner < Vagrant.plugin(2, :provisioner)
+      def initialize(machine, config)
+      end
+
+      def configure(root_config)
+      end
+
+      def provision
+      end
+
+      def cleanup
+      end
     end
   end
 end
